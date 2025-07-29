accc3f3f3245c80aad93c8a7052d369edb7a0f24
adzhanfarhan08 <adzhanfarhan@gmail.com>
2025-07-29 11:56:49 +0700

    initialized

--- first-commit.bat
+++
@@ -1,8 +0,0 @@
-git init
-git add .
-git commit -m "initialized"
-git branch -M main
-git remote add origin https://github.com/adzhanfarhan08/auto-install-software-package.git
-git push -u origin main
-
-pause
